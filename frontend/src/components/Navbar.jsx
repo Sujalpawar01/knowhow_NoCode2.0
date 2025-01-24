cc6b0cf536fb002d30d388aef5797b130b28001b
Monish609 <2023photosmail@gmail.com>
2025-01-24 17:37:06 +0530

    navbar

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+export const Navbar = () => {
+  return (
+    <div>
+      <nav>
+        <h1>RTO</h1>
+        <div>
+          <ul>
+            <li>Home</li>
+            <li>Application Tracking</li>
+            <li>Vehicle Registration</li>
+            <li>License</li>
+          </ul>
+        </div>
+        <div>Account</div>
+      </nav>
+    </div>
+  )
+}
